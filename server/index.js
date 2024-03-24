021780eae92c4ec9fba934b4d829b024b8c7f5ec
VempatiSiva456 <vempati.reddy@students.iiit.ac.in>
2024-03-25 02:52:52 +0530

    adding build file in client

--- server/index.js
+++ server/index.js
@@ -8,11 +8,9 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
 app.use(express.json());
+app.use(cors());
+app.use(express.static('./client/dist'));
 
 mongoose.connect(process.env.DB_CONNECTION_STRING, {
 })
@@ -21,6 +19,10 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, {
 
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
