4e4dab2e01249fc13024869d39255c33e6a34d22
VempatiSiva456 <vempati.reddy@students.iiit.ac.in>
2024-03-25 03:41:09 +0530

    path_resolved

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('./client/dist'));
+app.use(express.static('../client/dist'));
 
 mongoose.connect(process.env.DB_CONNECTION_STRING, {
 })
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
 app.listen(port, () => {
